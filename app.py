c92f9fa717dd03051fae881e898a476e7f85904d
KashyapGohil <kashyapgohil25@gmail.com>
2021-05-26 17:24:15 +0530

    Added files

--- app.py
+++ app.py
@@ -1,39 +1,13 @@
-import numpy as np, pandas as pd 
-from flask import Flask, render_template, request
-import cv2
-from werkzeug.utils import secure_filename
+import pandas as pd
+import numpy as np 
+import tensorflow as tf 
+#from tensorflow.keras.models import load_model
+#from tensorflow.keras.preprocessing import image
+#from tensorflow.keras.preprocessing.image import load_img
 import os
-import tensorflow as tf
+#os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+import streamlit as st 
+from PIL import Image
 
-app = Flask(__name__)
-model = tf.keras.models.load_model('models/model.h5')
-
-@app.route('/')
-def Home():
-    return render_template('index.html')
-
-@app.route('/predict', methods=['POST'])
-def predict():
-    if request.method == 'POST':
-        file = request.files['file']
-        basepath = os.path.dirname(__file__)
-        file_path = os.path.join(basepath, 'uploads', secure_filename(file.filename))
-        file.save(file_path)
-    
-    path = os.path.join(file_path)
-    print(path)
-    img_size=150
-    img_arr = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
-    resized_arr = cv2.resize(img_arr, (img_size, img_size))
-    resized_arr = np.array(resized_arr) / 255
-    resized_arr = resized_arr.reshape(-1, img_size, img_size, 1)
-    pred=model.predict_classes(resized_arr)[0][0]
-    if (pred==0):
-        pred='Pneumonia'
-    else:
-        pred='Normal'
-    return render_template('index.html', predict = pred)
-
-if __name__ == '__main__':
-    app.run(debug=True)
-s
+st.title("Chest X-Ray Detection(Pneumonia)")
+st.header("Identify Skin cancer")
